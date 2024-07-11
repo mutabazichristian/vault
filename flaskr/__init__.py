e3a25bb63570b923c471ccf7d8d0260fe5cd352e
mutabazichristian <c.mutabazi@alustudent.com>
2024-07-11 11:10:51 +0200

    Routed Login & Register pages

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,4 +1,15 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
 
-def get_all_users(connection):
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    msg = 'this is a message from the backend'
+    
+    return render_template('login.html', msg = msg)
+
+
+app.run()
