95323f12fdfd7fc8cef252e03bc6ea116aca1f67
mutabazichristian <c.mutabazi@alustudent.com>
2024-07-30 10:33:23 +0200

    debug

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
 from flaskr import create_app
-import os, logging
+import os
+import logging
 
 app = create_app()
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 10000))
     logging.info(f"Running on port {port}")
-    app.run(host="0.0.0.0", port=port)
+    app.run(port=port)
