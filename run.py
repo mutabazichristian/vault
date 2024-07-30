2fdcab03b4c93b23750ffee373b7cab8b0462d08
mutabazichristian <c.mutabazi@alustudent.com>
2024-07-30 10:24:02 +0200

    debug

--- run.py
+++ run.py
@@ -4,6 +4,6 @@ import os, logging
 app = create_app()
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
+    port = int(os.getenv('PORT', 10000))
     logging.info(f"Running on port {port}")
     app.run(host="0.0.0.0", port=port)
