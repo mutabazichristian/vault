c1828830a8ccdce0417008504d89689db038a9ed
mutabazichristian <c.mutabazi@alustudent.com>
2024-07-30 10:02:52 +0200

    debug

--- flaskr/db.py
+++ flaskr/db.py
@@ -11,7 +11,7 @@ def get_db_connection():
         password=os.getenv('DB_PASSWORD'),
         host=os.getenv('DB_HOST'),
         database=os.getenv('DB_NAME'),
-        port=int(os.getenv('DB_PORT'), 3306)
+        port=int(os.getenv('DB_PORT', 3306))
     )
 
     return connection
